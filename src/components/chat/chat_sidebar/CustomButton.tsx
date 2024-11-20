fa238910ab68931c1956681a0e60e0c834f0fdc8
Shuakami <Shuakami@sdjz.wiki>
2024-11-20 13:13:22 +0800

    fix(CustomButton):修复部署失败

--- src/components/chat/chat_sidebar/CustomButton.tsx
+++ src/components/chat/chat_sidebar/CustomButton.tsx
@@ -13,7 +13,7 @@ interface CustomButtonProps {
 const CustomButton: React.FC<CustomButtonProps> = ({ label, href = '#', selected, onClick }) => {
     return (
         <Link
-            href={href}
+            href={{ pathname: href }}
             className={`w-[185px] mx-3 text-sm mt-1 flex items-center space-x-2 rounded-md py-2 px-3 transition-colors duration-200 text-left text-black dark:text-white ${
                 selected ? 'bg-[#e0e0e0] dark:bg-[#333333]' : 'hover:bg-[#f0f0f0] dark:hover:bg-[#1e1e1e]'
             }`}
